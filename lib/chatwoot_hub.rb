5e96910a3e06b7bbb469224c4fe158fa39983ac3
caitano28 <caitano28@gmail.com>
2025-12-02 10:06:57 -0400

    fix premium plan

--- lib/chatwoot_hub.rb
+++ lib/chatwoot_hub.rb
@@ -21,13 +21,13 @@ class ChatwootHub
   def self.pricing_plan
     return 'community' unless ChatwootApp.enterprise?
 
-    InstallationConfig.find_by(name: 'INSTALLATION_PRICING_PLAN')&.value || 'community'
+    InstallationConfig.find_by(name: 'INSTALLATION_PRICING_PLAN')&.value || 'premium'
   end
 
   def self.pricing_plan_quantity
     return 0 unless ChatwootApp.enterprise?
 
-    InstallationConfig.find_by(name: 'INSTALLATION_PRICING_PLAN_QUANTITY')&.value || 0
+    InstallationConfig.find_by(name: 'INSTALLATION_PRICING_PLAN_QUANTITY')&.value || 10000000
   end
 
   def self.support_config

44b9930d706ddb01aaab6efd99c73c13f236b9d4
achadinhospromosdagleici-dev <achadinhospromosdagleici@gmail.com>
2026-06-21 19:15:25 -0300

    fix: rename table name references from channel_whatsapps to channel_whatsapp

--- app/models/channel/baileys.rb
+++ app/models/channel/baileys.rb
@@ -1,5 +1,5 @@
 class Channel::Baileys < Channel::Whatsapp
-  self.table_name = 'channel_whatsapps'
+  self.table_name = 'channel_whatsapp'
 
   validates :phone_number, presence: true, numericality: true
 
--- db/migrate/20250620000000_add_baileys_fields_to_channel_whatsapp.rb
+++ db/migrate/20250620000000_add_baileys_fields_to_channel_whatsapp.rb
@@ -1,7 +1,7 @@
 class AddBaileysFieldsToChannelWhatsapp < ActiveRecord::Migration[7.0]
   def change
-    add_column :channel_whatsapps, :qr_data_url, :string
-    add_column :channel_whatsapps, :baileys_webhook_secret, :string
-    add_index :channel_whatsapps, [:phone_number, :provider], unique: true
+    add_column :channel_whatsapp, :qr_data_url, :string
+    add_column :channel_whatsapp, :baileys_webhook_secret, :string
+    add_index :channel_whatsapp, [:phone_number, :provider], unique: true
   end
 end

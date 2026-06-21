5425715d5aac34ab46d78dfb0d3ec59e9c0a64d0
achadinhospromosdagleici-dev <achadinhospromosdagleici@gmail.com>
2026-06-21 16:25:29 -0300

    fix: rename AddBaileysFieldsToChannelWhatsapps to match filename

--- db/migrate/20250620000000_add_baileys_fields_to_channel_whatsapp.rb
+++ db/migrate/20250620000000_add_baileys_fields_to_channel_whatsapp.rb
@@ -1,4 +1,4 @@
-class AddBaileysFieldsToChannelWhatsapps < ActiveRecord::Migration[7.0]
+class AddBaileysFieldsToChannelWhatsapp < ActiveRecord::Migration[7.0]
   def change
     add_column :channel_whatsapps, :qr_data_url, :string
     add_column :channel_whatsapps, :baileys_webhook_secret, :string
